20ca4ad3aee42b1cf6fed554061fc2912216883e
takuto123 <103298294+takuto123@users.noreply.github.com>
2023-03-24 22:49:24 +0900

    保存

---
+++ myapp/Lesson12/index.php
@@ -0,0 +1,7 @@
+<?php
+$dbname = 'mysql';        // データベース名
+$host = 'localhost';        // ホスト
+$user = 'root';         // mysqlに接続するユーザー
+$mysqli = mysqli_connect('localhost', 'root', ' ', 'mydb');
+
+?>
