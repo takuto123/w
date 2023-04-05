d8b3443b4cf08712c0b0720690ac65169a0d2871
takuto123 <103298294+takuto123@users.noreply.github.com>
2023-04-05 20:23:34 +0900

    保存

--- myapp/Lesson12/index.php
+++ myapp/Lesson12/index.php
@@ -1,7 +1,29 @@
 <?php
-$dbname = 'mysql';        // データベース名
-$host = 'localhost';        // ホスト
-$user = 'root';         // mysqlに接続するユーザー
-$mysqli = mysqli_connect('localhost', 'root', ' ', 'mydb');
+
+    $dsn = 'mysql:dbname=sample;host=db';
+    $user = 'phper';
+    $pass = 'root';
+
+        
+    try{
+        $dbh = new PDO($dsn, $user, $pass);
+        
+        echo '接続成功<br>';
+
+        $sql = "SELECT first_name FROM users";
+        $stmt = $dbh -> query($sql);
+        $result = $stmt -> fetchAll(PDO::FETCH_COLUMN);
+        print_r($result);
+
+        $dbh = null;
+        // echo '123';
+        // exit;
+    } catch(PDOExeption $e){
+        echo '接続失敗'.$e->getMessage();
+        exit();
+    }
+
+ 
+
 
 ?>
