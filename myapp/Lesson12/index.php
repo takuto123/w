728d9e67916344fc70ba2b15fb61cb9484f69e68
takuto123 <103298294+takuto123@users.noreply.github.com>
2023-04-08 21:59:55 +0900

    保存

--- myapp/Lesson12/index.php
+++ myapp/Lesson12/index.php
@@ -8,12 +8,13 @@
     try{
         $dbh = new PDO($dsn, $user, $pass);
         
-        echo '接続成功<br>';
+        // echo '接続成功<br>';
 
-        // $sql = "SELECT first_name FROM users";苗字のみ表示の場合
-
+        // 1,SQLの準備
         $sql = "SELECT * FROM users";//useテーブル全て表示
+        // 2,SQLの実行
         $stmt = $dbh -> query($sql);
+        // 3,SQLの結果を受け取る
         $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
         print_r($result);
 
@@ -24,6 +25,27 @@
         echo '接続失敗'.$e->getMessage();
         exit();
     }
+
+    
+    function dbConnect(){
+        $dsn = 'mysql:dbname=sample;host=db';
+        $user = 'phper';
+        $pass = 'root';
+        try{
+            $dbh = new PDO($dsn, $user, $pass);
+            echo '接続成功';
+            $dbh = null;
+            return $result2;
+        } catch(PDOExeption $e){
+                echo '接続失敗'.$e->getMessage();
+                exit();
+        }
+    };
+    $deta = dbConnect();
+    echo $data;
+
+
+    
 
  
 
